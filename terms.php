cf78e80c1ff16b245c4b62f912edf233fa41fc57
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2020-09-20 18:38:31 +0100

    privacy_policy and terms content
    privacy_policy and terms content

---
+++ terms.php
@@ -0,0 +1,25 @@
+<div class="ind" >
+	
+	
+	
+<div class="titind" >Conditions D'utilisation</div>
+	
+<div class="bodyind">
+
+
+<div class='wanind'>    
+<div class='ntit'>Conditions D'utilisation</div>
+    
+<div class='nbody'>
+
+</div>
+    
+</div>
+    
+    
+    
+</div>
+	
+
+
+</div>
